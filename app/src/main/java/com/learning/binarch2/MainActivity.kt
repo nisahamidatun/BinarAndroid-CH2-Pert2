dd852eb0d2939d1a96c2f80d2cf85c922eb99583
Hamidatun Nisa <hamidatunnisa9d@gmail.com>
2023-09-01 12:29:14 +0700

    Initials Project

--- app/src/main/java/com/learning/binarch2/MainActivity.kt
+++ app/src/main/java/com/learning/binarch2/MainActivity.kt
@@ -2,10 +2,40 @@ package com.learning.binarch2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    // private var tvHelloWorld : TextView? = null
+
+    //1. Declare binding
+    private var binding : ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //2. initiate binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        //setContentView(R.layout.activity_main)
+        //3. change layout name into binding root
+        setContentView(binding?.root)
+        setTextHello()
     }
+
+    private fun setTextHello() {
+        // Old way
+
+        //tvHelloWorld = findViewById(R.id.tv_hello_world)
+        //tvHelloWorld?.text = "text text text" // Setter
+        //Toast.makeText(this, tvHelloWorld?.text, Toast.LENGTH_SHORT).show() //getter
+
+        //View binding
+        binding?.tvHelloWorld?.text = "text text text"
+
+        //context in android
+        // Application context -> whole app context
+        // Activity context -> Di activity mana
+    }
+
+
 }
